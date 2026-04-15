400d3b9410759b0bf785ea854e6e124755d85047
Valber <valbermarcelino999@gmail.com>
2026-04-15 16:59:37 -0300

    decorator completo monobloco

--- ex3/decorate/src/Main.java
+++ ex3/decorate/src/Main.java
@@ -8,8 +8,18 @@ class CafeExpresso implements Bebida {
     public double getCusto() { return 5.0; }
 }
 
+class Cappuccino implements Bebida {
+    public String getDescricao() { return "Cappuccino"; }
+    public double getCusto() { return 7.5; }
+}
+
+class Cha implements Bebida {
+    public String getDescricao() { return "Chá"; }
+    public double getCusto() { return 4.0; }
+}
+
 abstract class BebidaDecorator implements Bebida {
-    protected Bebida bebidaBase; // Composição [cite: 192, 264]
+    protected Bebida bebidaBase;
 
     public BebidaDecorator(Bebida bebida) {
         this.bebidaBase = bebida;
@@ -28,11 +38,34 @@ class Chantilly extends BebidaDecorator {
     public double getCusto() { return bebidaBase.getCusto() + 3.5; }
 }
 
+class Canela extends BebidaDecorator {
+    public Canela(Bebida b) { super(b); }
+    public String getDescricao() { return bebidaBase.getDescricao() + " + Canela"; }
+    public double getCusto() { return bebidaBase.getCusto() + 1.5; }
+}
+
+class CaldaChocolate extends BebidaDecorator {
+    public CaldaChocolate(Bebida b) { super(b); }
+    public String getDescricao() { return bebidaBase.getDescricao() + " + Calda de Chocolate"; }
+    public double getCusto() { return bebidaBase.getCusto() + 2.5; }
+}
+
 class ClienteDecorator {
     public static void main(String[] args) {
-        Bebida meuCafe = new Chantilly(new Leite(new CafeExpresso()));
 
-        System.out.println("Pedido: " + meuCafe.getDescricao());
-        System.out.println("Total: R$ " + meuCafe.getCusto());
+        Bebida pedido1 = new Chantilly(new Leite(new CafeExpresso()));
+        System.out.println("--- Pedido 1 ---");
+        System.out.println("Item: " + pedido1.getDescricao());
+        System.out.println("Total: R$ " + pedido1.getCusto());
+
+        Bebida pedido2 = new CaldaChocolate(new Canela(new Cappuccino()));
+        System.out.println("\n--- Pedido 2 ---");
+        System.out.println("Item: " + pedido2.getDescricao());
+        System.out.println("Total: R$ " + pedido2.getCusto());
+
+        Bebida pedido3 = new Cha();
+        System.out.println("\n--- Pedido 3 ---");
+        System.out.println("Item: " + pedido3.getDescricao());
+        System.out.println("Total: R$ " + pedido3.getCusto());
     }
 }
